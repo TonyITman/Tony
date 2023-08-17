c3605adb13d48658b47082f158b3ebff8fa4e04f
TonyITman <119666149+TonyITman@users.noreply.github.com>
2023-08-17 12:59:53 +0200

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,2 +1,8 @@
-
+window.onload = function() {
+  // Simulate some delay for demonstration purposes
+  setTimeout(function() {
+    document.querySelector('.preloader').style.display = 'none';
+    document.querySelector('.content').style.display = 'block';
+  }, 2000); // Change the delay time as needed
+};
   
